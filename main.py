0f39458bcd954a6c13d80a2d19947b0dc6ba4bc1
devlepa <juanestebanleonparra2@gmail.com>
2025-03-09 09:51:57 -0500

    añadiendo fastapi

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from verificar_crear import verificar_o_crear_bucket
 from subir import subir_s3
 from descargar import archivos_s3_descargados
 from scraping import scraping
+from fastapi import FastAPI
 
 path = "https://casas.mitula.com.co/casas/arriendo-bogota"
 headers = {"user-Agent": "Mozilla/5.0"}
@@ -16,6 +17,13 @@ s3 = boto3.client("s3", region_name="us-east-1")
 date_str = datetime.datetime.now().strftime("%Y-%m-%d")
 
     
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"message": "Bienvenido a mi API"}
+
   
 def crear_csv(datos):
     
@@ -23,29 +31,15 @@ def crear_csv(datos):
     df.to_csv("./datos/datos.csv")
 
 
-def lambda_handler(event, context):
-    """
-    Función Lambda que ejecuta la descarga y procesamiento de datos en AWS.
-    Se ejecuta automáticamente cada lunes a las 9 AM.
-    """
-
-    verificar_o_crear_bucket(bucket_name_1)
-
-    subir_s3(s3, path, headers, date_str, bucket_name_1)
-
-    archivos_s3_descargados()
-
-    crear_csv(scraping())
-
-
-
 
 
 if '__main__' == __name__:
+    import uvicorn
     verificar_o_crear_bucket(bucket_name=bucket_name_1)
     #time.sleep(10)
     subir_s3(s3, path, headers, date_str, bucket_name_1)
     #time.sleep(10)
     crear_csv(scraping(s3, bucket_name_1))
     #time.sleep(10)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
     
